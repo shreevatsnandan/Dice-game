9db16fe4c1d59790719605c27084cabfbccd598d
shreevatsnandan <117607123+shreevatsnandan@users.noreply.github.com>
2024-02-27 05:43:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Dice from './components/Dice';
 import Navbar from './components/Navbar';
 import './App.css';
-// import Body from './components/Body';
 
 function App() {
   return (
     <>        
     <Navbar />
     <Dice />
-    {/* <Body /> */}
     </>
   );
 }
